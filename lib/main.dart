35df20363f9a7d6055bda9cf7f83f22f62868f69
YanaLugina <yana4961@gmail.com>
2020-12-03 00:44:28 +1000

    main: used Navigator.push() and Navigator.pop()

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,37 @@ class _HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: RaisedButton(
-        onPressed: () {},
+        onPressed: () {
+          Route route = _myRoute();
+          Navigator.push(context, route);
+        },
         child: Text('Move to page 2')
+      ),
+    );
+  }
+}
+
+Route _myRoute() {
+  return MaterialPageRoute(
+    builder: (context) => _Page2()
+  );
+}
+
+class _Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page 2'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Go Back'),
+        ),
       ),
     );
   }
